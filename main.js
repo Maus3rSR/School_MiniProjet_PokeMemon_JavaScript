3e0c1d7a2d522b91775fa392eae32ee0aafe61b4
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-03-17 09:26:27 +0100

    ADD: revealPokemon

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
+const pokemonData = [
+  {
+    name: "charmander",
+    sprite:
+      "https://img.pokemondb.net/sprites/scarlet-violet/normal/charmander.png",
+  },
+  {
+    name: "squirtle",
+    sprite:
+      "https://img.pokemondb.net/sprites/scarlet-violet/normal/squirtle.png",
+  },
+  {
+    name: "bulbasaur",
+    sprite:
+      "https://img.pokemondb.net/sprites/scarlet-violet/normal/bulbasaur.png",
+  },
+  {
+    name: "pikachu",
+    sprite:
+      "https://img.pokemondb.net/sprites/scarlet-violet/normal/pikachu.png",
+  },
+];
+
+const pairOfPokemonIds = ["pikachu", "pikachu", "charmander", "charmander"];
 const gridHTML = document.querySelector("#grille_de_jeu");
-const bushListHTML = gridHTML.querySelectorAll(".box");
+const boxListHTML = gridHTML.querySelectorAll(".box");
 
 function hideBush(index) {
-  bushListHTML[index].style.display = "none";
+  boxListHTML[index].querySelector(".bush").style.display = "none";
+}
+
+function getPokemonData(pokemon_id) {
+  return pokemonData.find((pokemon) => pokemon.name === pokemon_id);
+}
+
+function createPokemonHtml({ sprite }) {
+  const pokemonHTML = document.createElement("img");
+  pokemonHTML.src = sprite;
+  pokemonHTML.classList.add("pokemon");
+
+  return pokemonHTML;
+}
+
+function revealPokemon(index) {
+  const pokemonHTML = createPokemonHtml(
+    getPokemonData(pairOfPokemonIds[index])
+  );
+  boxListHTML[index].appendChild(pokemonHTML);
 }
 
 hideBush(0);
+revealPokemon(0);
